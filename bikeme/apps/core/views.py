d78a79ac93b6e9fea701aa9c997e4c54bb07aade
crccheck <c@crccheck.com>
2014-03-15 16:36:30 -0500

    add a map

--- bikeme/apps/core/views.py
+++ bikeme/apps/core/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.core.serializers.json import DjangoJSONEncoder
 
 from . import models
 
@@ -9,3 +10,20 @@ class Landing(ListView):
 
 class MarketDetail(DetailView):
     model = models.Market
+
+    @staticmethod
+    def station_to_json(station):
+        return {
+            'name': station.name,
+            'latitude': station.latitude,
+            'longitude': station.longitude,
+            'street': station.street,
+            'zip': station.zip,
+        }
+
+    def get_context_data(self, **kwargs):
+        data = super(MarketDetail, self).get_context_data(**kwargs)
+        serializer = DjangoJSONEncoder()
+        stations = self.object.stations.all()
+        data['station_json'] = serializer.encode(map(self.station_to_json, stations))
+        return data
