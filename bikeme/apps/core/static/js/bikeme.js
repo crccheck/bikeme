b3af807024f60926204150534af203391cff544d
crccheck <c@crccheck.com>
2014-03-15 20:29:42 -0500

    delint js

--- bikeme/apps/core/static/js/bikeme.js
+++ bikeme/apps/core/static/js/bikeme.js
@@ -1,31 +1,36 @@
-var getIcon = function (stand) {
-  // http://leafletjs.com/reference.html#divicon
-  var filled = Math.round(stand.bikes / (stand.bikes + stand.docks) * 10);
-  return L.divIcon({
-    className: 'filled-marker',
-    html: '<div class="filled-' + filled + '">X</div>',
-    iconAnchornSize: null,
-    iconAnchor: null
+/* global $, L, stations: false */
+(function () {
+  'use strict';
+
+  var getIcon = function (stand) {
+    // http://leafletjs.com/reference.html#divicon
+    var filled = Math.round(stand.bikes / (stand.bikes + stand.docks) * 10);
+    return L.divIcon({
+      className: 'filled-marker',
+      html: '<div class="filled-' + filled + '">X</div>',
+      iconAnchornSize: null,
+      iconAnchor: null
+    });
+  };
+
+  // create a map in the "map" div, set the view to a given place and zoom
+  var map = L.map('map', {
+        scrollWheelZoom: false
+      }),
+      bounds = [];
+
+  $.each(stations, function (idx, stand) {
+    bounds.push([stand.latitude, stand.longitude]);
+    L.marker([stand.latitude, stand.longitude], {
+      icon: getIcon(stand),
+      title: stand.name + '\nBikes: ' + stand.bikes + '\nDocks: ' + stand.docks
+    }).addTo(map);
   });
-};
 
-// create a map in the "map" div, set the view to a given place and zoom
-var map = L.map('map', {
-      scrollWheelZoom: false
-    }),
-    bounds = [];
-
-$.each(stations, function (idx, stand) {
-  bounds.push([stand.latitude, stand.longitude]);
-  L.marker([stand.latitude, stand.longitude], {
-    icon: getIcon(stand),
-    title: stand.name + '\nBikes: ' + stand.bikes + '\nDocks: ' + stand.docks
+  // add an OpenStreetMap tile layer
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
-});
 
-// add an OpenStreetMap tile layer
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-map.fitBounds(bounds);
+  map.fitBounds(bounds);
+})();
