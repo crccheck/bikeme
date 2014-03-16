da9e42cf88830d4b3a214ef9f3a9576895d60538
crccheck <c@crccheck.com>
2014-03-16 17:52:02 -0500

    show active field in station admin

--- bikeme/apps/core/admin.py
+++ bikeme/apps/core/admin.py
@@ -7,13 +7,14 @@ admin.site.register(models.Market)
 
 
 class StationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'market', 'capacity', )
-    list_filter = ('market', )
+    list_display = ('name', 'market', 'capacity', 'active', )
+    list_filter = ('market', 'active', )
 admin.site.register(models.Station, StationAdmin)
 
 
 class SnapshotAdmin(admin.ModelAdmin):
     date_heirarchy = 'timestamp'
     list_display = ('station', 'bikes', 'docks', 'timestamp')
+    # WISHLIST select_related the list_filter queryset
     list_filter = ('station', )
 admin.site.register(models.Snapshot, SnapshotAdmin)
